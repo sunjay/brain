39e29fad3664195611142e8afa75423fe0038467
Sunjay Varma <varma.sunjay@gmail.com>
2017-01-11 17:05:33 -0500

    Reorganizing project structure

--- src/bin/brainfuck.rs
+++ src/bin/brainfuck.rs
@@ -1,9 +1,34 @@
+/// Brainfuck language interpreter
+/// As specified here: http://www.muppetlabs.com/~breadbox/bf/
+
+use std::env;
+use std::process;
+
 use std::io;
-use std::io::Read;
+use std::io::prelude::*;
+use std::fs::File;
 
 const BUF_SIZE: usize = 30_000;
 
-pub fn interpret(program: Vec<char>) {
+fn main() {
+    let argv = env::args().collect::<Vec<_>>();
+    assert!(argv.len() > 0);
+
+    if argv.len() != 2 {
+        println!("Usage: {} filename", argv[0]);
+        process::exit(1);
+    }
+
+    let f = File::open(argv[1].clone()).expect("Could not open file");
+
+    let program = f.bytes().map(
+        |c| c.expect("Could not read char") as char
+    ).collect::<Vec<char>>();
+
+    interpret(program);
+}
+
+fn interpret(program: Vec<char>) {
     let mut buffer = [0u8; BUF_SIZE];
 
     let mut p: usize = 0;
@@ -74,4 +99,3 @@ fn find_matching(program: &Vec<char>, start: usize) -> usize {
 
     current
 }
-
