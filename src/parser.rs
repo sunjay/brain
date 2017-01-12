b8e394dedaa8b817ccec4617c31ca0249100d744
Sunjay Varma <varma.sunjay@gmail.com>
2017-01-12 00:17:08 -0500

    Finally somehow got the parser to compile

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,4 @@
 use std::str;
-use nom::{IResult};
 
 const COMMENT_START: char = '#';
 const STRING_BOUNDARY: char = '"';
@@ -16,22 +15,22 @@ pub enum Expression {
     Text(String),
 }
 
-named!(pub parse< &str, Vec<Statement> >, many0!(statement));
+named!(pub parse< Vec<Statement> >, many0!(statement));
 
-named!(statement<&str, Statement>, ws!(alt!(
+named!(statement<Statement>, ws!(alt!(
     comment => {|content: &str| Statement::Comment(content.to_owned())} |
     assignment => {|(name, expr): (Option<&str>, Expression)| Statement::Assignment(name.map(|s| s.to_owned()), expr)}
 )));
 
-named!(comment<&str, &str>,
+named!(comment<&str>,
     do_parse!(
         char!(COMMENT_START) >>
-        content: take_until_and_consume_s!("\n") >>
+        content: take_until_and_consume!("\n") >>
         (str::from_utf8(content).unwrap())
     )
 );
 
-named!(assignment<&str, (Option<&str>, Expression)>,
+named!(assignment<(Option<&str>, Expression)>,
     do_parse!(
         name: assignment_name >>
         char!(ASSIGNMENT_OPERATOR) >>
@@ -40,30 +39,40 @@ named!(assignment<&str, (Option<&str>, Expression)>,
     )
 );
 
-named!(assignment_name< &str, Option<&str> >,
-    opt!(
-        take_while1_s!(is_valid_assignment_name_char)
+named!(assignment_name< Option<&str> >,
+    map!(
+        opt!(assignment_name_impl),
+        |n: Option<&'a [u8]>| n.and_then(|s| str::from_utf8(s).ok())
     )
 );
 
-named!(expression<&str, Expression>,
+// Without this separation, type inference fails
+named!(assignment_name_impl<&[u8]>,
+    take_while1_s!(is_valid_assignment_name_char)
+);
+
+named!(expression<Expression>,
     alt!(
-        expr_text => {|text| Expression::Text(text.to_owned())}
+        expr_text => {|text: &str| Expression::Text(text.to_owned())}
     )
 );
 
-named!(expr_text<&str, &str>,
-    delimited!(
-        char!(STRING_BOUNDARY),
-        take_while_s!(is_string_boundary),
-        char!(STRING_BOUNDARY)
+named!(expr_text<&str>,
+    map!(
+        delimited!(
+            char!(STRING_BOUNDARY),
+            take_while!(is_string_boundary),
+            char!(STRING_BOUNDARY)
+        ),
+        |s: &'a [u8]| str::from_utf8(s).unwrap()
     )
 );
 
-fn is_valid_assignment_name_char(c: char) -> bool {
-    c.is_alphanumeric() || c == '_'
+fn is_valid_assignment_name_char(c: u8) -> bool {
+    let ch = c as char;
+    ch.is_alphanumeric() || ch == '_'
 }
 
-fn is_string_boundary(c: char) -> bool {
-    c == STRING_BOUNDARY
+fn is_string_boundary(c: u8) -> bool {
+    c as char == STRING_BOUNDARY
 }
