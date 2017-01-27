224ae32e1108582e5052791293bda4798d855883
Sunjay Varma <varma.sunjay@gmail.com>
2017-01-27 18:17:48 -0500

    Changing the default delay in the debugger

--- brainfuck-visualizer/app/models/settings.jsx
+++ brainfuck-visualizer/app/models/settings.jsx
@@ -1,7 +1,7 @@
 const {createRecord} = require('./model');
 
 const SettingsRecord = createRecord({
-  DEFAULT_BRAINFUCK_COMMAND: 'brainfuck --debug --delay 50',
+  DEFAULT_BRAINFUCK_COMMAND: 'brainfuck --debug --delay 10',
 }, (constants) => ({
   command: constants.DEFAULT_BRAINFUCK_COMMAND,
   file: undefined,
