cc2660f0a568260111ba1c9353bd098763647b88
Sunjay Varma <varma.sunjay@gmail.com>
2017-02-28 14:37:54 -0500

    Properly exposing modules as modules instead of exposing their inner methods

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -1,19 +1,14 @@
 pub mod scope;
 pub mod item_type;
+pub mod program;
+pub mod statement;
+pub mod declaration;
+pub mod type_definition;
+pub mod expression;
 
 mod operation;
-mod program;
-mod statement;
-mod declaration;
-mod type_definition;
-mod expression;
 
 pub use self::operation::*;
-pub mod self::program::*;
-pub mod self::statement::*;
-pub mod self::declaration::*;
-pub mod self::type_definition::*;
-pub mod self::expression::*;
 
 use parser::Program;
 
