bf296aa6dc2875bdb473e5433d6d2a2784cfe5ba
Sunjay Varma <varma.sunjay@gmail.com>
2017-02-28 14:54:57 -0500

    Making it possible to define varadic functions

--- src/operations/item_type.rs
+++ src/operations/item_type.rs
@@ -5,17 +5,34 @@ use memory::Size;
 use super::operation::Operation;
 use super::scope::{ScopeItem, ScopeStack};
 
+/// The arguments that will get passed to a function
+/// Distinct to the function's actual type because these are
+/// ScopeItems not type defintions
+/// Arguments can be assumed to match the type of that function
 pub type FuncArgs = Vec<ScopeItem>;
 
+/// Definition of a function's type
 #[derive(Clone)]
 pub struct FunctionTypeDef {
-    args: Vec<ItemType>,
+    args: Vec<FuncArgType>,
     return_type: Box<ItemType>,
+}
+
+#[derive(Clone)]
+pub enum FuncArgType {
+    /// A single argument of the given type
+    Arg(ItemType),
+    /// Any number of arguments of the given type
+    Variadic(ItemType),
 }
 
 /// An item is anything that can be declared
 #[derive(Clone)]
 pub enum ItemType {
+    /// Any type
+    ///TODO: Replace with type bounds when generics are implemented in #45
+    Any,
+
     /// A struct has a single definition with any number of
     /// fields and generics
     /// Structs can have impls which contain methods for that
@@ -35,7 +52,7 @@ pub enum ItemType {
         /// Generates operations that represent calling the
         /// function with the given arguments
         operations: Rc<Fn(FuncArgs, ScopeStack) -> Vec<Operation>>,
-    }
+    },
 }
 
 impl ItemType {
