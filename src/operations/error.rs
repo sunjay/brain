281a9e384cb5950c0aea1c64436aa1213911feee
Sunjay Varma <varma.sunjay@gmail.com>
2017-03-04 03:15:33 -0500

    Adding span information to AST (tests broken)

--- src/operations/error.rs
+++ src/operations/error.rs
@@ -1,13 +1,19 @@
 use super::operation::Operations;
 
-use parser::Identifier;
+use parser::{Identifier, Span};
 
 pub type OperationsResult = Result<Operations, Error>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Error {
     /// Unresolved name: `name`
-    UnresolvedName(Identifier),
+    UnresolvedName {
+        name: Identifier,
+        span: Span,
+    },
     /// `name` is not a valid type
-    InvalidType(Identifier),
+    InvalidType {
+        name: Identifier,
+        span: Span,
+    },
 }
