767ad06815cace7a85f589e33a56cc52197c242d
Sunjay Varma <varma.sunjay@gmail.com>
2017-03-03 19:41:19 -0500

    Adding some errors into unimplemented spots

--- src/operations/type_definition.rs
+++ src/operations/type_definition.rs
@@ -10,11 +10,9 @@ pub fn resolve_type_id(type_def: TypeDefinition, scope: &ScopeStack) -> Result<T
         // We return the first declaration found because we want to use the latest definition
         // of the type that we are defining
         Name {name} => scope.lookup(&name).first().ok_or_else(|| {
-            //TODO: No type found that is associated with that name
-            unimplemented!();
+            Error::UnresolvedName(name.clone())
         }).and_then(|it| it.type_id().ok_or_else(|| {
-            //TODO: Attempt to use a non-type name as a type
-            unimplemented!();
+            Error::InvalidType(name)
         })),
         //TODO: Deal with infinitely sized (self-referential) types
         Array {type_def, size} => unimplemented!(),
