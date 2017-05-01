713d0db2cb209d0a19903c222ff9fe21c05d9b10
Sunjay Varma <varma.sunjay@gmail.com>
2017-05-01 16:28:16 -0400

    Some simple errors that could obviously be returned in the expression code

--- src/operations/expression/mod.rs
+++ src/operations/expression/mod.rs
@@ -8,7 +8,8 @@ pub use self::call::call;
 use parser::Expression;
 use memory::MemoryBlock;
 
-use operations::OperationsResult;
+use operations::{Error, OperationsResult};
+use operations::item_type::ItemType;
 use operations::scope::{TypeId, ScopeStack, ArraySize};
 
 use self::identifier::{store_identifier, store_identifier_array};
@@ -30,6 +31,13 @@ pub fn into_operations(
         Expression::Identifier(name) => store_identifier(scope, name, target_type, target),
         Expression::Number(value) => store_number(scope, value, target_type, target),
         Expression::Call {method, args} => call_with_exprs(scope, *method, args, target_type, target),
+        Expression::ByteLiteral(bytes) => Err(Error::MismatchedTypes {
+            expected: scope.get_type(target_type).clone(),
+            found: ItemType::Array {
+                item: Some(scope.primitives().u8()),
+                size: Some(bytes.len()),
+            },
+        }),
         _ => unimplemented!(),
     }
 }
@@ -49,6 +57,13 @@ pub fn into_operations_array(
     match expr {
         Expression::ByteLiteral(bytes) => store_byte_literal(scope, bytes, item_type, size, target),
         Expression::Identifier(name) => store_identifier_array(scope, name, item_type, size, target),
+        Expression::Number(value) => Err(Error::MismatchedTypes {
+            expected: ItemType::Array {
+                item: Some(item_type),
+                size: Some(size),
+            },
+            found: scope.get_type(scope.primitives().u8()).clone(),
+        }),
         _ => unimplemented!(),
     }
 }
